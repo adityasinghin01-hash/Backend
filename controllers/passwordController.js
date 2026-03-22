300569377ba08b49eca310f1b98881431fefdd91
Aditya Singh <aditya.singh.in01@gmail.com>
2026-03-22 14:26:52 +0530

    fix: generate 5-digit OTP instead of 6-digit

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -181,7 +181,7 @@ const sendOtp = async (req, res, next) => {
         }
 
         // Generate 6-digit OTP
-        const rawOtp = Math.floor(100000 + Math.random() * 900000).toString();
+        const rawOtp = Math.floor(10000 + Math.random() * 90000).toString();
         user.otpCode = hashToken(rawOtp);
         user.otpExpiry = Date.now() + 15 * 60 * 1000; // 15 minutes
 
